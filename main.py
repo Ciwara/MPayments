d702fad6742710efad340c70b935e2a95ee6e861
Ibrahima Fadiga <ibfadiga@gmail.com>
2026-01-30 18:27:44 +0000

    Update main.py to use PyQt6 instead of PyQt5 and modify QApplication execution method for compatibility.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 
 from Common.cmain import cmain
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 
+
 if __name__ == "__main__":
     if cmain():
-        sys.exit(app.exec_())
+        sys.exit(app.exec())
